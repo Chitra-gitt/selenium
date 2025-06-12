bc5d0bd70be51e72060bf14f09dff0aad0d1dc7e
siddharth <siddharth@siddharths-mbp>
2025-06-12 18:48:25 +0530

    read and write data

--- SeleniumTutorials_Chitra/data_driven_testing/read_data.py
+++ SeleniumTutorials_Chitra/data_driven_testing/read_data.py
@@ -24,3 +24,26 @@ o/p-
 4
 
 '''
+
+import openpyxl
+
+filepath = "/Users/chitra/Desktop/Testdata-DDT.xlsx" 
+my_workbook = openpyxl.load_workbook(filepath)
+my_active_sheet = my_workbook.active # to get the currently active sheet
+my_active_sheet = my_workbook["Sheet1"]
+total_rows = my_active_sheet.max_row
+print(total_rows)
+
+
+total_columns = my_active_sheet.max_column
+print(total_columns)
+
+
+
+
+
+
+
+
+
+
---
+++ SeleniumTutorials_Chitra/data_driven_testing/write_data.py
@@ -0,0 +1,8 @@
+'''
+Created on 11 Jun 2025
+
+@author: Chitra
+'''
+import openpyxl
+filepath = "/Users/chitra/Desktop/Testdata-DDT.xlsx" 
+my_workbook = openpyxl.load_workbook(filepath)
