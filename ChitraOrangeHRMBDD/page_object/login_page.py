a1223e61e06e0a25cf4291ec1b0324db50c358dd
siddharth <siddharth@siddharths-mbp>
2025-06-21 14:12:49 +0530

    page object model --> login and base page

---
+++ ChitraOrangeHRMBDD/page_object/login_page.py
@@ -0,0 +1,22 @@
+'''
+Created on 20 Jun 2025
+
+@author: Chitra
+
+'''
+from page_objects.base_page import BasePage
+class LoginPage(BasePage):
+    
+    def _init_(self,driver):
+        super.__init__(driver)
+        
+    def enter_username(self):
+        pass
+    
+    def entrr_password(self):
+        pass
+    
+    def click_login(self):
+        pass
+    
+    
